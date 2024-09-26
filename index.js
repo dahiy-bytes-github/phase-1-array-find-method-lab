7ba118840a9935fa2ec10a73189029ed7d19d921
Abdullahi <aadahiye@gmail.com>
2024-09-27 00:34:48 +0300

    Completes lab on array-find-method

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records) {
+    // Use the find() method to locate the first win
+    const winRecord = records.find(record => record.result === "W");
+    
+    // Return the year if a win is found, otherwise return undefined
+    return winRecord ? winRecord.year : undefined;
+  }
+  
